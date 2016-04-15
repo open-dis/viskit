279c6f5723046c80162e18bfaac6f158e19679ae
brutzman <brutzman@0de750ab-c0a9-dc11-9d74-0011435b89d6>
2016-04-15 07:58:15 +0000

    dialog for modifying project metadata
    git-svn-id: https://eos.nps.edu/wcm/Viskit/trunk@2992 0de750ab-c0a9-dc11-9d74-0011435b89d6

---
+++ src/viskit/view/dialog/ProjectMetadataDialog.java
@@ -0,0 +1,37 @@
+package viskit.view.dialog;
+
+import viskit.model.GraphMetadata;
+
+import javax.swing.JFrame;
+
+/**
+ * OPNAV N81 - NPS World Class Modeling (WCM)  2004 Projects
+ * MOVES Institute
+ * Naval Postgraduate School, Monterey, CA
+ * www.nps.edu
+ * @author Mike Bailey
+ * @since Aug 19, 2004
+ * @since 1:35:07 PM
+ * @version $Id$
+ */
+public class ProjectMetadataDialog extends MetadataDialog {
+
+    private static MetadataDialog dialog;
+    
+    public static boolean showDialog(JFrame f, GraphMetadata graphMetadata) {
+        if (dialog == null) {
+            dialog = new ProjectMetadataDialog(f, graphMetadata);
+        } else {
+            dialog.setGraphMetadata(f, graphMetadata);
+        }
+        dialog.setVisible(true);
+        // above call blocks
+        return modified;
+    }
+
+    ProjectMetadataDialog(JFrame f, GraphMetadata graphMetadata) {
+        super(f, graphMetadata, "Project Properties");
+        remove(this.runtimePanel);  // only for assembly
+        pack();
+    }
+}
