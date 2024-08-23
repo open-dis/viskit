57f269e9ce0cf954d2601fdb116d07c5c1b22d2a
git <tdnorbra@nps.edu>
2024-08-23 07:29:16 -0700

    [Terry N.] unused import

--- src/viskit/view/ClassPanel.java
+++ src/viskit/view/ClassPanel.java
@@ -4,7 +4,6 @@ import javax.swing.*;
 import javax.swing.filechooser.FileFilter;
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.File;
 import viskit.VGlobals;
 import viskit.util.FindClassesForInterface;
