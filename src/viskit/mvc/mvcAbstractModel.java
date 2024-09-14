91ceb442073f37b984d9e4557c4eb380e81194ab
git <tdnorbra@nps.edu>
2024-09-14 10:53:11 -0700

    [Terry N.] comment

--- src/viskit/mvc/mvcAbstractModel.java
+++ src/viskit/mvc/mvcAbstractModel.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Abstract root class of the model hierarchy.  Provides basic notification behavior.
+ * Abstract root class of the model hierarchy. Provides basic notification behavior.
  *
  * From an article at www.jaydeetechnology.co.uk
  *
