4575ff055e8abd54d59131fde473fc11fdb48536
brutzman <brutzman@nps.edu>
2025-04-25 18:46:41 -0700

    clear warning, add @Override

--- src/viskit/ViskitLayoutRedesign.java
+++ src/viskit/ViskitLayoutRedesign.java
@@ -931,6 +931,7 @@ public class ViskitLayoutRedesign extends javax.swing.JFrame {
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
             public void run() {
                 new ViskitLayoutRedesign().setVisible(true);
             }
